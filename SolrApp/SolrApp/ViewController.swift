fe4bd5134a4abe716ab7d82613805818e1ab1777
Pawanpreet93 <pawanpreet@outlook.com>
2015-07-23 14:18:50 +0530

    Created an XCode Project.

---
+++ SolrApp/SolrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolrApp
+//
+//  Created by Pawanpreet Singh on 23/07/15.
+//  Copyright (c) 2015 Kiota Labs Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
